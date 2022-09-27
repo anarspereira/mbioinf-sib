a862f4a6c382f1f4d724ec424150dce0738e9755
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2022-09-27 11:09:22 +0100

    [ADD] csv and data_file read and write methods (incomplete)

---
+++ src/si/io/csv.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def read_csv (filename: str, sep: str, features: bool, label: bool) -> object:
+    return pd.read_csv
+
+def write_csv (filename: str, dataset: object, sep: str, features: bool, label: bool) -> object:
+    return pd.write_csv
---
+++ src/si/io/data_file.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def read_data_file (filename: str, sep: str, label: bool) -> object:
+    return np.genfromtxt
+
+def write_csv (filename: str, dataset: object, sep: str, label: bool) -> object:
+    return np.savetxt
