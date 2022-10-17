a5fdffe86bcd1dd54e10a125935e473eb269d607
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2022-10-17 11:49:55 +0100

    [UPDATE] credits

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Ana Rafaela Pereira"
-__credits__ = ["Vítor Pereira, Fernando Cruz, Ana Rafaela Pereira"]
+__credits__ = ["Ana Rafaela Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Ana Rafaela Pereira"
