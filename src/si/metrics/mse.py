d80f0f9eb76065a0542c2055c657b8f48ce3185b
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2023-01-01 22:13:45 +0000

    [ADD] mse derivative

--- src/si/metrics/mse.py
+++ src/si/metrics/mse.py
@@ -14,3 +14,18 @@ def mse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
         O Mean Squared Error do modelo entre y_true e y_pred
     """
     return np.sum((y_true - y_pred) ** 2) / (len(y_true) * 2)
+
+
+def mse_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    """
+    Retorna o Meaan Squared Error derivativo do modelo para o y_pred.
+
+    :param y_true: np.ndarray
+        Labels reais do dataset
+    :param y_pred: np.ndarray
+        Labels estimadas do dataset
+
+    :return: mse_derivative: np.ndarray
+        O derivative Mean Squared Error
+    """
+    return -2 * (y_true - y_pred) / (len(y_true) * 2)
