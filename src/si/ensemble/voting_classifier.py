71f27e26d3b18f4810318132d8f93800e45b2ef7
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2023-01-01 18:05:48 +0000

    [DONE] voting and stacking classifier and eval 6

--- src/si/ensemble/voting_classifier.py
+++ src/si/ensemble/voting_classifier.py
@@ -5,15 +5,25 @@ from si.metrics.accuracy import accuracy
 
 
 class VotingClassifier:
+    """
+    Ensemble classifier that uses the majority vote to predict the class labels.
+
+    Parameters
+    ----------
+    models : array-like, shape = [n_models]
+        Different models for the ensemble.
+    """
 
     def __init__(self, models: list):
         """
-
-        :param models: Lista de conjunto de modelos
+        Initialize the ensemble classifier.
+        Parameters
+        ----------
+        models: array-like, shape = [n_models]
+            Different models for the ensemble.
         """
         # parameters
         self.models = models
-        pass
 
     def fit(self, dataset: Dataset) -> 'VotingClassifier':
         """
@@ -30,18 +40,13 @@ class VotingClassifier:
             The fitted model
         """
         for model in self.models:
-            node.fit(dataset)
-        return self
+            model.fit(dataset)
 
-    # helper function - não precisa de estar aqui. está dentro porque não é mais precisa noutro lado
-    def _get_majority_vote(pred: np.ndarray) -> int:
-        # get the most comon label
-        labels, counts = np.unique(pred, return_counts=True)
-        return labels[np.argmax(counts)]
+        return self
 
     def predict(self, dataset: Dataset) -> np.array:
         """
-        Predict the output of the dataset.
+        Predict the output of the dataset - predict class labels for samples in X.
 
         Vai modelo a modelo, eleitor a eleitor, perguntar "quem achas que vai ganhar a eleição".
         Pega neles todos, escolhe o que tem maior contagem e é isso que ele retorna.
@@ -56,9 +61,16 @@ class VotingClassifier:
         predictions: np.array
             The predictions of the dataset
         """
+        def _get_majority_vote(pred: np.ndarray) -> int: # helper function - não precisa de estar aqui. está dentro
+            # porque não é mais precisa noutro lado
+            # get the most comon label
+            labels, counts = np.unique(pred, return_counts=True)
+            return labels[np.argmax(counts)]
+
         predictions = np.array([model.predict(dataset) for model in self.models]).transpose()
         # caso não fizer a transposta, posso colocar axis=0
-        return np.apply_along_axis(self._get_majority_vote, axis=1, arr=predictions) # aplicar o método linha por linha (axis=1)
+        return np.apply_along_axis(_get_majority_vote, axis=1, arr=predictions) # aplicar o método linha por linha
+        # (axis=1)
 
     def score(self, dataset: Dataset) -> float:
         """
@@ -70,3 +82,30 @@ class VotingClassifier:
         """
         predictions = self.predict(dataset)
         return accuracy(dataset.y, predictions)
+
+
+if __name__ == '__main__':
+    # imports
+    from si.data.dataset_module import Dataset
+    from si.model_selection.split import train_test_split
+    from si.neighbors.knn_classifier import KNNClassifier
+    from si.linear_model.logistic_regression import LogisticRegression
+
+    # load and split the dataset
+    dataset_ = Dataset.from_random(600, 100, 2)
+    dataset_train, dataset_test = train_test_split(dataset_, test_size=0.2)
+
+    # initialize the KNN and Logistic classifier
+    knn = KNNClassifier(k=3)
+    lg = LogisticRegression(l2_penalty=1, alpha=0.001, max_iter=1000)
+
+    # initialize the Voting classifier
+    voting = VotingClassifier([knn, lg])
+
+    voting.fit(dataset_train)
+
+    # compute the score
+    score = voting.score(dataset_test)
+    print(f"Score: {score}")
+
+    print(voting.predict(dataset_test))
